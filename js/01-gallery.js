3639e36ffbc1ee8382a701229299c46d5bfd5d1a
Svitlana <wsv3ta@gmail.com>
2023-10-02 21:11:49 +0200

    1 task added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+for (const { original, preview, description } of galleryItems) {
+  gallery.innerHTML += `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+}
 
-console.log(galleryItems);
+gallery.addEventListener("click", selectImage);
+
+function selectImage(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`
+  );
+  instance.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+}
