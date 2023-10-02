614df771e3d0326c110cab3f8e764d164c7bb6db
Svitlana <wsv3ta@gmail.com>
2023-10-03 01:49:23 +0200

    added task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+for (const { preview, original, description } of galleryItems) {
+  gallery.innerHTML += `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+}
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250
+});
